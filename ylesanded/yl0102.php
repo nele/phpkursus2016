fff9e65dfeeb0511a7f2595689775f086f88f490
nele <nele.naris@gmail.com>
2016-05-02 12:40:23 +0300

    Update yl0102.php

--- ylesanded/yl0102.php
+++ ylesanded/yl0102.php
@@ -1,5 +1,30 @@
 <?php
+/*Ülesanne 0102
+Järgnevas ülesandes tohib kasutada vaid IF tingimust ja FOR tsükklit. Kuvada ühe FOR tsükkliga välja järgmine arvude jada järgmisel kujul: 
+-11
+-10
+-9
+-8
+-7
+-6
+-5
+-4
+-3
+3
+4
+5
+6
+7
+8
+9
+10
+*/
 for ($i=-11;$i<11;$i=$i+1){
-echo "$i"."<br>";
+  if ($i<-2){ 
+    echo "$i"."<br>";
+  }
+  if ($i>2){ 
+    echo "$i"."<br>";
+  }
 }
 ?>
