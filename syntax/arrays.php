919a2111c0d7d86b867d48cd30d7df7d1bd232c9
juku@tmk.edu.ee <juku@tmk.edu.ee>
2016-05-02 19:23:03 +0300

    minu katse veelkord

--- syntax/arrays.php
+++ syntax/arrays.php
@@ -1,14 +1,51 @@
 <?php
-$drinks[]="Piim";
-$drinks[]="Kohv";
-$drinks[]="Mahl";
+
+$drinks[]="piim";
+$drinks[]="kohv";
+$drinks[]="mahl";
 $drinks[]="Coca-Cola";
-$drinks[]="Energiajook";
+$drinks[]="energiajook";
 
 
 echo $drinks[0];
 
-echo"<p>";
+echo"<br>";
+echo"<br>";
 print_r($drinks);
+
+
+$rand1 = rand(0, sizeof($drinks)-1);
+$rand2 = rand(0, sizeof($drinks)-1);
+
+echo "<br>";
+echo "<br>";
+echo "Isa lemmikjook on $drinks[$rand1].";
+
+echo "<br>";
+echo "<br>";
+echo "Minu lemmikjook on $drinks[$rand2].";
+
+echo "<hr>";
+echo "<br>";
+echo "<br>";
+
+echo "<ul>";
+for ($i = 0; $i <= sizeof($drinks)-1; $i++)
+{
+    echo "<li>$drinks[$i] ";
+    echo "- " . rand(2,8) . " eurot";
+    echo "</li>";
+}
+echo "</ul>";
+
+
+foreach ($drinks as $key => $val)
+{
+ $counter++;
+ echo "$counter) Massiivi element indeksiga $key väärtus on $val<br>";
+    
+   
+}
+
 
 ?>
