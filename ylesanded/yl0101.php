c2449e39137c34ff202adc84fe3bb3c3bfbe4dbb
nele <nele.naris@gmail.com>
2016-05-02 12:58:14 +0300

    Update yl0101.php

--- ylesanded/yl0101.php
+++ ylesanded/yl0101.php
@@ -1,6 +1,20 @@
 <?php
+/*
+Ülesanne 0101
+Järgnevas ülesandes tohib kasutada vaid IF tingimust ja FOR tsükklit. Arvude üleühe kuvamiseks kasutage lisamuutujat. Kuvada ühe FOR tsükkliga välja järgmine arvude jada järgmisel kujul: 
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+*/
 $a=2;
 for ($i=1;$i<20;$i=$i+$a) {
-echo "$i"."<br>";
+  echo "$i"."<br>";
 }
 ?>
