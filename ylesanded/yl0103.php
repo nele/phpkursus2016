c620fb639bf0d6b9b46873b9c02c73cf405650e3
nele <nele.naris@gmail.com>
2016-05-02 12:54:44 +0300

    Update yl0103.php

--- ylesanded/yl0103.php
+++ ylesanded/yl0103.php
@@ -1,1 +1,21 @@
-
+<?php
+/*
+Ülesanne 0103
+Järgnevas ülesandes tohib kasutada vaid IF tingimust, mooduli võtmist (mod %) ja FOR tsükklit. Kuvada ühe FOR tsükkliga välja järgmine arvude jada järgmisel kujul: 
+100
+200
+300
+400
+500
+600
+700
+800
+900
+1000
+*/
+for($i=100;$i<1100;$i=$i+1){
+	if ($i % 100 == 00){
+		echo "$i <br>";
+	}
+}
+?>
