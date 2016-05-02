d0373e7da71c515accf98868b7f7f2f5a6f34f15
nele <nele.naris@gmail.com>
2016-05-02 17:23:31 +0300

    Update yl0103.php

--- ylesanded/yl0103.php
+++ ylesanded/yl0103.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/*nele, 2. mai 2016
 Ülesanne 0103
 Järgnevas ülesandes tohib kasutada vaid IF tingimust, mooduli võtmist (mod %) ja FOR tsükklit. Kuvada ühe FOR tsükkliga välja järgmine arvude jada järgmisel kujul: 
 100
