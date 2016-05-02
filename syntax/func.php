3c085bf947940c18e353d456eafa77a45bd5d745
juku@tmk.edu.ee <juku@tmk.edu.ee>
2016-05-02 19:40:38 +0300

    minu katse veelkorrrrr

--- syntax/func.php
+++ syntax/func.php
@@ -1,7 +1,23 @@
 <?php
 
 
+function getname($name)
+{
+    $names ="Koer   123;
+kass    2837423
+kukk    83274
+kana 428384";
+    
+    $rows = explode("\n",$names);
+    
+    print_r($rows);
+    
+    return $count;
+    
+}
 
+#Funktsioon niisama ei käivitu. Peame funktsiooni esile kutsuma.
 
+getname("koer");
 
 ?>
