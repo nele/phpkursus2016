637ce4731da78264f63df5e8bc5521f961cd6a9d
juku@tmk.edu.ee <juku@tmk.edu.ee>
2016-05-02 19:54:31 +0300

    minu katse 2. mai kell 19.56

--- syntax/func.php
+++ syntax/func.php
@@ -3,14 +3,24 @@
 
 function getname($name)
 {
-    $names ="Koer   123;
+    $names ="Koer    123;
 kass    2837423
 kukk    83274
 kana 428384";
     
     $rows = explode("\n",$names);
     
-    print_r($rows);
+  //  print_r($rows);
+    
+    
+    
+    foreach($rows as $key => $val)
+    {
+        $temp = explode("    ", $val);
+        $surname[$temp[0]] = $temp[1];
+         } 
+    print_r($surname);
+
     
     return $count;
     
