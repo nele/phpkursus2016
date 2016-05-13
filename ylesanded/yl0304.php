7a5a31be49a4b8e78299583c60f902b16d7fd4a5
nele <nele.naris@gmail.com>
2016-05-13 16:06:33 +0300

    Add files via upload
    kodused ülesanded nr 3 natuke poolikult

---
+++ ylesanded/yl0304.php
@@ -0,0 +1,61 @@
+
+<?php
+/*
+Ülesanne 0304
+
+Kasutades arikmeetilist tehet moodul teha funktsioon nimega "number_info", mille sisendargumendiks on täisarv ja mis: 
+
+1. Teatab kasutajale kas tegu on paaris või paaritu arvuga.
+	Mooduli näide:
+	12%2=0
+	13%2=1
+	14%2=0
+2. Teatab kasutajale kas arv on suurem või väiksem kui 10.
+3. Teatab kasutajale kas arv on suurem või väiksem kui 100.
+4. Näitab kasutajale arvu ruudus.
+5. Näitab kasutajale arvust ruutjuurt.
+
+Käivita funktsioon 5 korda sisestades arvudeks: 1, 8, 9, 15, 200
+*/
+
+
+function number_info($num) 
+{
+  
+  echo "<br>Arv on: ".$num. ".";  
+  if ($num%2==0)
+       { 
+    echo " Tegu on täisarvuga. ";
+  }      
+      
+    elseif ($num%2==1)
+       { 
+    echo " Tegu ei ole täisarvuga. ";
+  } 
+  
+  if ($num<10)
+       {
+    echo " See arv on väiksem kui 10.";
+    } 
+    elseif ($num>10)
+       { 
+    echo " See on arv on suurem kui 10.";
+  } 
+    if ($num<100)
+       {
+    echo " See arv on väiksem kui 100.";
+    } 
+    elseif ($num>100)
+       { 
+    echo " See on arv on suurem kui 100.";
+  } 
+  echo " Antud arv ruudus on ".($num*$num).".";
+  echo " Ruutjuur antud arvust on ".round(pow($num, 1/2),3).".";
+}
+number_info(1);
+number_info(8);
+number_info(9);
+number_info(15);
+number_info(200);
+
+?>
---
+++ ylesanded/yl0305.php
@@ -0,0 +1,41 @@
+
+<?php
+/*
+Ülesanne 0305
+
+Teha funktsioon nimega "string_info". Funktsiooni sisendargumendiks olgu teksti string ($string). Funktsioon üleb:
+
+1. Mis on selle stringi esimene täht ja viimane täht. Funktsioon "strlen" leiab stringi pikkuse ja "echo $string[0]" kuvab esimese tähe.
+2. Mitu tähte stringis on (strlen).
+3. Kogu string tehaks läbiva väiketähega (strtolower) ja väljastatakse.
+4. Kogu string tehaks läbiva suurtähega (strtoupper) ja väljastatakse.
+5.
+ Tsükliga loetakse kokku ja kuvatakse ekraanile mitu "a" tähte stringis esineb.
+*/
+
+function string_info($str) 
+{
+  
+  echo "Stringi esimene täht on: ".$str[0].". ";
+  
+  echo "<br>Stringis ".$str. " on ".strlen($str)." tähte. ";
+
+  echo  "<br>Antud string väikeste tähtedega on järgmine ".strtolower($str).".";
+  echo  "<br>Antud string suurte tähtedega on järgmine ".strtoupper($str).".";
+ $arv=0;
+    for ($i=0; $i < strlen($str); $i++ ){
+    
+
+        if ($str[$i]=='a') { 
+        $arv=$arv+1;
+    
+        } 
+  
+  
+    } 
+  echo  "<br>Stringis ".$str. " on a tähti ".$arv."."; 
+}
+
+string_info("Phpkursus2016");
+
+?>
