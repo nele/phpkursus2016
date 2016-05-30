d19f91dd56cd25c4dbf590f5a70de65073920f29
nele <nele.naris@gmail.com>
2016-05-30 15:11:43 +0300

    Add files via upload
    Projekt, 30. mai 2016, PHP kursus 2016 kevad.

---
+++ projekt/info.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Nele Naris, 30. mai 2016, PHP kursus 2016 kevad
+ * 
+ * See fail kuvab infot projekti kohta.
+ * 
+*/
+echo '
+<html>
+<title>Millionaire info</title>
+<body>
+
+<h2>Programmi Who Wants to be a Millionaire infofail</h2>
+
+<b>Millega tegu & kuidas kasutada?</b><br><br>
+Kuna oli soovituslik teha mäng, siis otsustasin nii öelda digitaliseerida ühe kodus olevatest mitmekümnest lauamängust.
+<br>Programmiks vajalik materjal (küsimused) on saadud kodus olevast lauamängust 
+Who Wants to be a Millionaire? 
+<br><br>
+Ehk siis kasutaja saab neli vastusevarianti ja peab nende vahel valima ning selle, 
+mida ta õigeks peab.<br><br>
+Kui on rohkem aega saaks lisada ka näiteks e-maili sõbrale ja fifty/fifty nupu.
+<br>
+<br>
+NB! Ei kavatse seda kuskile müüma hakata. Küsimused võtsin lauamängust lihtsalt aja kokkuhoiu mõttes, kohe kui kursus läbi, kustutan ka mängu internetist.
+<br><br>
+Mäng on praegu üleval ka minu domeenis aadressil  <a href="http://www.naris.ee">naris.ee</a> .
+<br><br>
+Allalaetud failidest tuleks käivitada index.html.
+<p>
+
+
+
+<b>Autorist ja programmist</b><br>
+autor: Nele Naris <br>
+rühm: php kursus 2016 kevad <br>
+juhendaja: A. Klooster<br>
+PHP projekt <br>
+30. mai 2016
+</body>
+</html>
+';
+
+?>
